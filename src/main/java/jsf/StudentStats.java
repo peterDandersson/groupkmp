5f3b8932178ae0e3f2c107fb1fca223cf9640848
elev <elev>
2017-04-28 16:36:04 +0200

    student-stats

---
+++ src/main/java/jsf/StudentStats.java
@@ -0,0 +1,66 @@
+package jsf;
+
+
+import ejb.AttendanceService;
+import ejb.CourseService;
+import ejb.StudentService;
+import javafx.util.Pair;
+import jpa.Attendance;
+import jpa.Course;
+import jpa.StudentCourse;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@ManagedBean
+@SessionScoped
+public class StudentStats {
+
+    @EJB
+    AttendanceService attendanceService;
+    @EJB
+    CourseService courseService;
+    @EJB
+    StudentService studentService;
+
+    @ManagedProperty(value="#{userBean}")
+    private UserBean userBean;
+
+    public List<Course> studentCourses(){
+
+        return studentService.getCourses(userBean.getUser().getId());
+    }
+
+    public String getAttendance(Course course){
+
+        int day = 0;
+        int attendance = 0;
+
+        StudentCourse studentCourse = course.getStudentCourse(userBean.getUser().getId());
+        Set<Attendance> attendanceList = studentCourse.getAttendances();
+        for (Attendance attendance1 : attendanceList){
+            attendance += attendance1.isPresent() ? 1 : 0;
+        }
+        day = attendanceList.size();
+
+        return "" + attendance + "/" + day;
+    }
+
+    public List<Pair<Course, String>> attendanceList(){
+        Pair<Course,String> pair;
+        List<Pair<Course, String>> list = new ArrayList();
+
+
+        for (Course course : studentCourses()){
+            String s = getAttendance(course);
+            Pair<Course, String> p = new Pair<>(course, s);
+            list.add(p);
+        }
+        return list;
+    }
+}
